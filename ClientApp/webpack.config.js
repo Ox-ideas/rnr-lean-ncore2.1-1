733056af5a545272423e354558358efe239122a5
ox-ideas <sunnygb.8@gmail.com>
2018-09-06 20:26:47 +0800

    -- create clientapp, css, js folder, add webpack.config.js, style.css, app-1.js

---
+++ ClientApp/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const _mini_css = require ('mini-css-extract-plugin');
+
+module.exports = (env = {}, argv = {}) => {
+  
+  const isProd = argv.mode === 'production';
+  const config = {
+    mode: argv.mode || 'development', // we default to development when no 'mode' arg is passed
+    entry: {
+      main: './js/app-1.js'
+    }, 
+    output: {
+      filename: 'js/[name].bundle.js',
+      path: path.resolve(__dirname, '../wwwroot')
+    },
+    plugins: [
+      new _mini_css({
+        filename: 'css/[name].bundle.css'
+      })
+    ],
+    module:  {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            isProd ? _mini_css.loader : 'style-loader',
+            'css-loader' 
+          ]
+        }
+      ]
+    }
+  }
+  
+  return config;
+};
