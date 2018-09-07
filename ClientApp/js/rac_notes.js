6b5eac631e80e960f96353acc7bd9547d81ce4d6
ox-ideas <sunnygb.8@gmail.com>
2018-09-07 15:16:21 +0800

    -- add react app

---
+++ ClientApp/js/rac_notes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import racApp from '../react-notes/app';
+
+ReactDOM.render(
+    <racApp />,
+    document.getElementById('react-notes-app')
+);
